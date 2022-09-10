4ed870f2357e6e06d91d52ecce7c9ff26fab10e4
kipraschi <kipraschi@gmail.com>
2022-09-11 00:08:41 +0300

    add card showing/hiding events on button clicks

---
+++ script.js
@@ -0,0 +1,108 @@
+const addBookCard = document.querySelector(".add-book-card");
+const formCard = document.querySelector(".new-book");
+const addButton = document.querySelector(".plusBTN");
+const saveButton = document.querySelector(".addBTN");
+const removeButton = document.querySelector(".removeBTN");
+
+formCard.classList.add("hidden");
+
+addButton.addEventListener("click", showNewForm);
+saveButton.addEventListener("click", addBookToLibrary);
+removeButton.addEventListener("click", hideForm);
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+	this.Title = title;
+	this.Author = author;
+	this.Pages = pages;
+	this.Read = read;
+}
+
+Book.prototype.info = function toggleStatus(bool) {
+	if(bool) this.read = true;
+};
+
+function addBookToLibrary() {
+	let title = document.querySelector("#title").value;
+	let author = document.querySelector("#author").value;
+	let pages = document.querySelector("#pages").value;
+	let read = document.querySelector("#read");
+	let newBook = new Book(title, author, pages, read);
+	if (title != 0 && author != 0 && pages != 0) {
+		myLibrary.push(newBook);
+		displayLibrary();
+		hideForm();
+	} else alert("Please fill in all of the fields to add a book.");
+	// addBookCard.classList.remove("hidden");
+}
+
+function displayLibrary() {
+	Array.from(document.querySelectorAll(".book-display")).forEach((element) =>
+		element.remove()
+	);
+	myLibrary.forEach((book, index) => {
+		displayBook(book, index);
+	});
+}
+
+function displayBook(book, index) {
+	const bookCard = createCard();
+	bookCard.dataset.index = index;
+	const heading = document.createElement("h2");
+	heading.textContent = `Book ${index + 1}`;
+
+	const infoContainer = document.createElement("div");
+	infoContainer.classList.add("book-info-container");
+
+	//create a label for each property of the book
+	//display the property value
+	//and add them to one container
+	for (const [key, value] of Object.entries(book)) {
+		let label = document.createElement("div");
+		label.classList.add("label");
+		label.textContent = key;
+		let property = document.createElement("div");
+		property.classList.add("book-property");
+		key == "Read" ? (property = value.cloneNode(true)) : (property.textContent = value);
+		infoContainer.append(label, property);
+	}
+
+	const buttonContainer = document.createElement("div");
+	buttonContainer.classList.add("buttons");
+
+	//add a "Remove" button to the book card
+	const removeButton = document.createElement("button");
+	removeButton.classList.add("removeBTN");
+	removeButton.addEventListener("click", removeBook);
+	buttonContainer.appendChild(removeButton);
+
+	//add the label-property container and button container to the a new card
+	bookCard.append(heading, infoContainer, buttonContainer);
+}
+
+function createCard() {
+	const newCard = document.createElement("div");
+	newCard.classList.add("card", "book-display");
+	formCard.insertAdjacentElement("beforebegin", newCard);
+	return newCard;
+}
+
+function showNewForm() {
+	formCard.classList.remove("hidden");
+	formCard.querySelector("#title").value = "";
+	formCard.querySelector("#author").value = "";
+	formCard.querySelector("#pages").value = "";
+	formCard.querySelector("#read").checked = false;
+	// addBookCard.classList.add("hidden");
+}
+
+function hideForm() {
+	formCard.classList.add("hidden");
+}
+
+function removeBook() {
+	let thisCard = this.parentNode.parentNode;
+	myLibrary.splice(thisCard.dataset.index, 1);
+	displayLibrary();
+}
